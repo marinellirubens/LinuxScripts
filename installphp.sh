c3a0e34d7fbee793503a1cb927ff057d7681776f
rubens <rubens@localhost.localdomain>
2016-10-03 20:59:15 -0300

    backup

---
+++ installphp.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo apt-get install -y curl
+sudo apt-get install -y mysql-server php5-mysql mysql-client
+sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
+sudo nano /etc/apache2/mods-enabled/dir.conf
+
+
+
