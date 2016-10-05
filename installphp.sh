eb9b929ca4ea4f9a8a0ce171004856c376b731a0
rubens <rubens@localhost.localdomain>
2016-10-05 01:51:22 -0300

    up

--- installphp.sh
+++ installphp.sh
@@ -4,6 +4,9 @@ sudo apt-get install -y curl
 sudo apt-get install -y mysql-server php5-mysql mysql-client
 sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
 sudo nano /etc/apache2/mods-enabled/dir.conf
-
+sudo apt-get install phpmyadmin
+sudo cp -r /usr/share/phpmyadmin/ /var/www/html/phpmyadmin
+sudo chmod 777 -R  /var/www/html
+sudo service apache2 restart
 
 
